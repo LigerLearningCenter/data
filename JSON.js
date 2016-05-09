2fae07526357ffa1cb6b480fab82d02d80233aa4
SreynithLiger <sreynith.s@Ligercambodia.org>
2016-05-09 14:51:18 +0700

    Check it out!

--- JSON.js
+++ JSON.js
@@ -1,22 +1,22 @@
-sfunction Events(description, industry, agriculture, consumerServices, infrastructure, education, tourism, ecology) {
+function Events(description, industry, agriculture, consumerServices, infrastructure, education, tourism, ecology) {
     this.description = description;
     this.industry = industry;
-    this.agriculture = agriculture;
-    this.consumerServices = consumerServices;
-    this.infrastructure = infrastructure;
-    this.education = education;
-    this.tourism = tourism;
-    this.ecology = ecology;
+    this.agr = agriculture;
+    this.con = consumerServices;
+    this.inf = infrastructure;
+    this.edu = education;
+    this.tou = tourism;
+    this.eco = ecology;
 }
 var randomEvents = [{
     description: {},
-    industry: {},
-    agriculture: {},
-    consumerServices: {},
-    infrastructure: {},
-    education: {},
-    tourism: {},
-    ecology: {}
+    ind: {},
+    agr: {},
+    con: {},
+    inf: {},
+    edu: {},
+    tou: {},
+    eco: {}
 }];
 //industry sector
 randomEvents[0].ind.RI01 = new Events("More jobs in clothing industry", 0.02, -0.02, 0.02, 0, 0, 0, 0);
@@ -51,34 +51,34 @@ randomEvents[0].ind.RI029 = new Events("Export of agro industry products decreas
 randomEvents[0].ind.RI030 = new Events("New health and safety at work regulations", -0.06, -0.02, 0, 0, 0.02, 0, 0);
 
 //aagriculture sector
-randomEvents[0].agri.RI01 = new Events("Quality award for Cambodian rice", 0, 0.02, 0, 0, 0, 0, 0);
-randomEvents[0].agri.RI02 = new Events("Increase in rice export", 0, 0.04, 0, 0.02, 0, 0, 0);
-randomEvents[0].agri.RI03 = new Events("Decrease in rice export", 0, -0.04, 0, -0.02, 0, 0, 0);
-randomEvents[0].agri.RI04 = new Events("International investment in Cambodian rice fields", 0, 0.02, 0, 0, 0, 0, 0);
-randomEvents[0].agri.RI05 = new Events("Price of rice rises", 0, 0.04, -0.02, 0, 0, 0, 0);
-randomEvents[0].agri.RI06 = new Events("Price of rice falls", 0, -0.04, 0.02, 0, 0, 0, 0);
-randomEvents[0].agri.RI07 = new Events("Quality award for Cambodian rubber", 0, 0.02, 0, 0, 0, 0, 0);
-randomEvents[0].agri.RI08 = new Events("Increase in rubber export", 0, 0.04, 0, 0.02, 0, 0, 0);
-randomEvents[0].agri.RI09 = new Events("Decrease in rubber export", 0, -0.04, 0, -0.02, 0, 0, 0);
-randomEvents[0].agri.RI010 = new Events("International investment in Cambodian rubber", 0, 0.02, 0, 0, 0, 0, 0);
-randomEvents[0].agri.RI011 = new Events("Price of rubber rises", -0.02, 0.04, 0, 0, 0, 0, 0);
-randomEvents[0].agri.RI012 = new Events("Price of rubber falls", 0.02, -0.04, 0, 0, 0, 0, 0);
-randomEvents[0].agri.RI013 = new Events("Quality award for Cambodian cassava", 0, 0.02, 0, 0, 0, 0, 0);
-randomEvents[0].agri.RI014 = new Events("Increase in cassava export", 0, 0.04, 0, 0.02, 0, 0, 0);
-randomEvents[0].agri.RI015 = new Events("Decrease in cassava export", 0, -0.04, 0, -0.02, 0, 0, 0);
-randomEvents[0].agri.RI016 = new Events("International investment in Cambodian cassava", 0, 0.02, 0, 0, 0, 0, 0);
-randomEvents[0].agri.RI017 = new Events("Price of cassava rises", 0, 0.04, -0.02, 0, 0, 0, 0);
-randomEvents[0].agri.RI018 = new Events("Price of cassava falls", 0, -0.04, 0.02, 0, 0, 0, 0);
-randomEvents[0].agri.RI019 = new Events("Quality award for Cambodian pepper", 0, 0.02, 0, 0, 0, 0, 0);
-randomEvents[0].agri.RI020 = new Events("Increase in pepper export", 0, 0.04, 0, 0.02, 0, 0, 0);
-randomEvents[0].agri.RI021 = new Events("Decrease in pepper export", 0, -0.04, 0, -0.02, 0, 0, 0);
-randomEvents[0].agri.RI022 = new Events("Quality award for Cambodian palm sugar", 0, 0.02, 0, 0, 0, 0, 0);
-randomEvents[0].agri.RI023 = new Events("Increase in palm sugar export", 0, 0.04, 0, 0.02, 0, 0, 0);
-randomEvents[0].agri.RI024 = new Events("Decrease in palm sugar export", 0, -0.04, 0, -0.02, 0, 0, 0);
-randomEvents[0].agri.RI025 = new Events("Price of palm sugar rises", -0.02, 0.04, -0.02, 0, 0, 0, 0);
-randomEvents[0].agri.RI026 = new Events("Price of palm sugar falls", 0.02, -0.04, 0.02, 0, 0, 0, 0);
-randomEvents[0].agri.RI027 = new Events("Price of fish rise", 0, 0.06, -0.06, 0, 0, 0, 0);
-randomEvents[0].agri.RI028 = new Events("Price of fish fall", 0, -0.06, 0.06, 0, 0, 0, 0);
-randomEvents[0].agri.RI029 = new Events("Local goverment supports fisheries", 0, 0.04, 0, 0, 0, 0, 0.02);
-randomEvents[0].agri.RI030 = new Events("Goverment supports fisheries", 0, 0.06, 0, 0, 0, 0, 0.04);
-console.log(randomEvents[0].agri.RI01) //print out line 54
+randomEvents[0].agr.RI01 = new Events("Quality award for Cambodian rice", 0, 0.02, 0, 0, 0, 0, 0);
+randomEvents[0].agr.RI02 = new Events("Increase in rice export", 0, 0.04, 0, 0.02, 0, 0, 0);
+randomEvents[0].agr.RI03 = new Events("Decrease in rice export", 0, -0.04, 0, -0.02, 0, 0, 0);
+randomEvents[0].agr.RI04 = new Events("International investment in Cambodian rice fields", 0, 0.02, 0, 0, 0, 0, 0);
+randomEvents[0].agr.RI05 = new Events("Price of rice rises", 0, 0.04, -0.02, 0, 0, 0, 0);
+randomEvents[0].agr.RI06 = new Events("Price of rice falls", 0, -0.04, 0.02, 0, 0, 0, 0);
+randomEvents[0].agr.RI07 = new Events("Quality award for Cambodian rubber", 0, 0.02, 0, 0, 0, 0, 0);
+randomEvents[0].agr.RI08 = new Events("Increase in rubber export", 0, 0.04, 0, 0.02, 0, 0, 0);
+randomEvents[0].agr.RI09 = new Events("Decrease in rubber export", 0, -0.04, 0, -0.02, 0, 0, 0);
+randomEvents[0].agr.RI010 = new Events("International investment in Cambodian rubber", 0, 0.02, 0, 0, 0, 0, 0);
+randomEvents[0].agr.RI011 = new Events("Price of rubber rises", -0.02, 0.04, 0, 0, 0, 0, 0);
+randomEvents[0].agr.RI012 = new Events("Price of rubber falls", 0.02, -0.04, 0, 0, 0, 0, 0);
+randomEvents[0].agr.RI013 = new Events("Quality award for Cambodian cassava", 0, 0.02, 0, 0, 0, 0, 0);
+randomEvents[0].agr.RI014 = new Events("Increase in cassava export", 0, 0.04, 0, 0.02, 0, 0, 0);
+randomEvents[0].agr.RI015 = new Events("Decrease in cassava export", 0, -0.04, 0, -0.02, 0, 0, 0);
+randomEvents[0].agr.RI016 = new Events("International investment in Cambodian cassava", 0, 0.02, 0, 0, 0, 0, 0);
+randomEvents[0].agr.RI017 = new Events("Price of cassava rises", 0, 0.04, -0.02, 0, 0, 0, 0);
+randomEvents[0].agr.RI018 = new Events("Price of cassava falls", 0, -0.04, 0.02, 0, 0, 0, 0);
+randomEvents[0].agr.RI019 = new Events("Quality award for Cambodian pepper", 0, 0.02, 0, 0, 0, 0, 0);
+randomEvents[0].agr.RI020 = new Events("Increase in pepper export", 0, 0.04, 0, 0.02, 0, 0, 0);
+randomEvents[0].agr.RI021 = new Events("Decrease in pepper export", 0, -0.04, 0, -0.02, 0, 0, 0);
+randomEvents[0].agr.RI022 = new Events("Quality award for Cambodian palm sugar", 0, 0.02, 0, 0, 0, 0, 0);
+randomEvents[0].agr.RI023 = new Events("Increase in palm sugar export", 0, 0.04, 0, 0.02, 0, 0, 0);
+randomEvents[0].agr.RI024 = new Events("Decrease in palm sugar export", 0, -0.04, 0, -0.02, 0, 0, 0);
+randomEvents[0].agr.RI025 = new Events("Price of palm sugar rises", -0.02, 0.04, -0.02, 0, 0, 0, 0);
+randomEvents[0].agr.RI026 = new Events("Price of palm sugar falls", 0.02, -0.04, 0.02, 0, 0, 0, 0);
+randomEvents[0].agr.RI027 = new Events("Price of fish rise", 0, 0.06, -0.06, 0, 0, 0, 0);
+randomEvents[0].agr.RI028 = new Events("Price of fish fall", 0, -0.06, 0.06, 0, 0, 0, 0);
+randomEvents[0].agr.RI029 = new Events("Local goverment supports fisheries", 0, 0.04, 0, 0, 0, 0, 0.02);
+randomEvents[0].agr.RI030 = new Events("Goverment supports fisheries", 0, 0.06, 0, 0, 0, 0, 0.04);
+console.log(randomEvents[0]) //print out line 54
