e4c311863efa58d1405b56913b1d2206e7e41405
SreynithLiger <sreynith.s@Ligercambodia.org>
2016-05-10 14:42:23 +0700

    test commit

---
+++ influencing social mood in sectors/agriculture.js
@@ -0,0 +1,20 @@
+function Events(description, industry, agriculture, consumerServices, infrastructure, education, tourism, ecology) {
+    this.description = description;
+    this.industry = industry;
+    this.agriculture = agriculture;
+    this.consumerServices = consumerServices;
+    this.infrastructure = infrastructure;
+    this.education = education;
+    this.tourism = tourism;
+    this.ecology = ecology;
+}
+var randomEvents = [{
+    description: {},
+    industry: {},
+    agriculture: {},
+    consumerServices: {},
+    infrastructure: {},
+    education: {},
+    tourism: {},
+    ecology: {}
+}];
