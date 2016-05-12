4eed0be2155f7de185cb178f2e5c6e89e03ca65d
SreynithLiger <sreynith.s@Ligercambodia.org>
2016-05-12 15:30:05 +0700

    change agri and ind sectors

--- sectors/Industry.js
+++ sectors/Industry.js
@@ -1,63 +1,32 @@
-function Events(description, money, mood) {
-    this.description = description;
-    this.money = {
-        industry: money[0],
-        agriculture: money[1],
-        consumerServices: money[2],
-        infrastructure: money[3],
-        education: money[4],
-        tourism: money[5],
-        ecology: money[6]
-    }
-    this.mood = {
-        industry: mood[0],
-        agriculture: mood[1],
-        consumerServices: mood[2],
-        infrastructure: mood[3],
-        education: mood[4],
-        tourism: mood[5],
-        ecology: mood[6]
-    }
+var industryEvents = [];
 
-}
-var randomEvents = [{
-    description: {},
-    industry: {},
-    agriculture: {},
-    consumerServices: {},
-    infrastructure: {},
-    education: {},
-    tourism: {},
-    ecology: {}
-}];
-//industry sector
-randomEvents[0].industry.RI01 = new Events("More jobs in clothing industry", [0.02, -0.02, 0.02, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI02 = new Events("Layoffs in clothing industry", [-0.02, 0, 0, 0, 0, 0, 0], [-1, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI03 = new Events("Export of clothes increases", [0.06, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI04 = new Events("Export of clothes decreases", [-0.06, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI05 = new Events("International investment in clothing industry", [0.06, 0, 0.02, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI06 = new Events("More jobs in timber production industry", [0.02, 0, 0.02, 0, 0.02, 0, 0], [1, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI07 = new Events("Layoffs in timber production industry", [-0.02, 0, 0, 0, 0, 0, 0], [-1, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI08 = new Events("Export of timber increases", [0.06, 0, 0, 0.02, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI09 = new Events("Export of timber decreases", [-0.06, 0, 0, -0.02, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI010 = new Events("International investment in timber industry", [0.06, 0, 0.02, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI011 = new Events("More jobs in residential construction industry", [0.02, -0.02, 0.02, 0, 0.02, 0, 0], [1, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI012 = new Events("Layoffs in residential construction industry", [-0.02, 0, 0, 0, 0, 0, 0], [-1, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI013 = new Events("Increased demand for residential housing", [0.06, 0, 0, 0.04, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI014 = new Events("Decreased demand for residential housing", [-0.06, 0, 0, -0.04, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI015 = new Events("International investment in construction industry", [0.06, 0, 0.02, 0.02, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI016 = new Events("More jobs in mining industry", [0.02, -0.02, 0.02, 0, 0.04, 0, 0], [1, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI017 = new Events("Layoffs in mining industry", [-0.02, 0, 0, 0, 0, 0, 0], [-1, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI018 = new Events("Demand for mineral resources rases", [0.06, 0, 0, 0.06, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI019 = new Events("Demand for mineral resources falls", [-0.06, 0, 0, -0.06, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI020 = new Events("International investment in mineral resources industry", [0.06, 0, 0.02, 0.04, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI021 = new Events("New offshore oil fields discovered", [0.04, 0, 0, 0, 0, 0, -0.04], [1, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI022 = new Events("New natural gas offshore fields discovered", [0.04, 0, 0, 0, 0, 0, -0.04], [1, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI023 = new Events("New oil fields become operational", [0.06, 0, 0, 0.04, 0, -0.02, -0, 06], [0, 0, 0, 0, 0, 0, -1]);
-randomEvents[0].industry.RI024 = new Events("New natural gas fields become operational", [0.06, 0, 0, 0.04, 0, -0.02, -0.06], [0, 0, 0, 0, 0, 0, -1]);
-randomEvents[0].industry.RI025 = new Events("More jobs in agro industry", [0.02, 0.02, 0.02, 0, 0.04, 0, 0], [1, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI026 = new Events("Layoffs in agro industry", [-0.02, -0.02, 0, 0, 0, 0, 0], [-1, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI027 = new Events("International investment in agro industry", [0.06, 0.06, 0.02, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI028 = new Events("Export of agro industry products increases", [-0.06, 0.04, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI029 = new Events("Export of agro industry products decreases", [0.06, -0.04, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]);
-randomEvents[0].industry.RI030 = new Events("New health and safety at work regulations", [-0.06, -0.02, 0, 0, 0.02, 0, 0], [1, 1, 0, 0, 0, 0, 0]);
+agriculture.push(new Events("RI01", "More jobs in clothing industry", [0.02, -0.02, 0.02, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI02", "Layoffs in clothing industry", [-0.02, 0, 0, 0, 0, 0, 0], [-1, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI03", "Export of clothes increases", [0.06, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI04", "Export of clothes decreases", [-0.06, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI05", "International investment in clothing industry", [0.06, 0, 0.02, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI06", "More jobs in timber production industry", [0.02, 0, 0.02, 0, 0.02, 0, 0], [1, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI07", "Layoffs in timber production industry", [-0.02, 0, 0, 0, 0, 0, 0], [-1, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI08", "Export of timber increases", [0.06, 0, 0, 0.02, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI09", "Export of timber decreases", [-0.06, 0, 0, -0.02, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI010", "International investment in timber industry", [0.06, 0, 0.02, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI011", "More jobs in residential construction industry", [0.02, -0.02, 0.02, 0, 0.02, 0, 0], [1, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI012", "Layoffs in residential construction industry", [-0.02, 0, 0, 0, 0, 0, 0], [-1, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI013", "Increased demand for residential housing", [0.06, 0, 0, 0.04, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI014", "Decreased demand for residential housing", [-0.06, 0, 0, -0.04, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI015", "International investment in construction industry", [0.06, 0, 0.02, 0.02, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI016", "More jobs in mining industry", [0.02, -0.02, 0.02, 0, 0.04, 0, 0], [1, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI017", "Layoffs in mining industry", [-0.02, 0, 0, 0, 0, 0, 0], [-1, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI018", "Demand for mineral resources rases", [0.06, 0, 0, 0.06, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI019", "Demand for mineral resources falls", [-0.06, 0, 0, -0.06, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI020", "International investment in mineral resources industry", [0.06, 0, 0.02, 0.04, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI021", "New offshore oil fields discovered", [0.04, 0, 0, 0, 0, 0, -0.04], [1, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI022", "New natural gas offshore fields discovered", [0.04, 0, 0, 0, 0, 0, -0.04], [1, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI023", "New oil fields become operational", [0.06, 0, 0, 0.04, 0, -0.02, -0, 06], [0, 0, 0, 0, 0, 0, -1]));
+agriculture.push(new Events("RI024", "New natural gas fields become operational", [0.06, 0, 0, 0.04, 0, -0.02, -0.06], [0, 0, 0, 0, 0, 0, -1]));
+agriculture.push(new Events("RI025", "More jobs in agro industry", [0.02, 0.02, 0.02, 0, 0.04, 0, 0], [1, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI026", "Layoffs in agro industry", [-0.02, -0.02, 0, 0, 0, 0, 0], [-1, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI027", "International investment in agro industry", [0.06, 0.06, 0.02, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI028", "Export of agro industry products increases", [-0.06, 0.04, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI029", "Export of agro industry products decreases", [0.06, -0.04, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]));
+agriculture.push(new Events("RI030", "New health and safety at work regulations", [-0.06, -0.02, 0, 0, 0.02, 0, 0], [1, 1, 0, 0, 0, 0, 0]));
